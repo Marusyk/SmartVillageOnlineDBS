e5e445aa500e40f8eabcc9f0f00e22809aa8a14e
Roma <romamarusyk@gmail.com>
2015-06-17 01:05:30 +0300

    Fill table Country

---
+++ 50.Data/Country.sql
@@ -0,0 +1,5 @@
+insert into Country (Name) values ('Україна')
+insert into Country (Name) values ('Польща')
+insert into Country (Name) values ('Білорусія')
+insert into Country (Name) values ('Молдова')
+
