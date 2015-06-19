d1a7d0d8cf9b3ae2cabb91b1e804924d48cd1231
Roma <romamarusyk@gmail.com>
2015-06-20 02:56:11 +0300

    Modified table House

--- 15.Tables/House.sql
+++ 15.Tables/House.sql
@@ -1,7 +1,7 @@
 IF NOT EXISTS (select * from dbo.sysobjects where id = object_id(N'[House]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
 BEGIN
 	CREATE TABLE [House]
-	(	[HouseID] [int] IDENTITY(1,1) NOT NULL
+	(	[ID] [int] IDENTITY(1,1) NOT NULL
 	,	[HouseNr] [varchar](10) NOT NULL
 	,	[KadastrNr] [varchar](10)
 	,	[BuildNr] [varchar](10) NOT NULL
@@ -15,7 +15,7 @@ BEGIN
 	,	[LastUpdUS] [varchar](50) NOT NULL	
 	, CONSTRAINT [SYS_House_PKY] PRIMARY KEY CLUSTERED 
 		  (
-			  [HouseID] ASC
+			  [ID] ASC
 		  ) WITH  FILLFACTOR = 95  ON [PRIMARY]
 	)   ON [PRIMARY]
 END
