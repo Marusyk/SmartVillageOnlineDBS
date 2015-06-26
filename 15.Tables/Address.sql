9ab84dc520866095f20950656b01e01782c07526
Roma <romamarusyk@gmail.com>
2015-06-26 23:00:26 +0300

    Clean 2 tables

--- 15.Tables/Address.sql
+++ 15.Tables/Address.sql
@@ -1,13 +1,5 @@
 IF NOT EXISTS (select * from dbo.sysobjects where id = object_id(N'[Address]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
 BEGIN
-	CREATE TABLE [Address]
-	(	[AddressID] [int] IDENTITY(1,1) NOT NULL
-	,	[CityID] [int] NOT NULL
-	,	[StreetID] [int]
-	, CONSTRAINT [SYS_Address_PKY] PRIMARY KEY CLUSTERED 
-		  (
-			  [AddressID] ASC
-		  ) WITH  FILLFACTOR = 95  ON [PRIMARY]
-	)   ON [PRIMARY]
+ // TO DO
 END
 GO
--- 15.Tables/House.sql
+++ 15.Tables/House.sql
@@ -1,22 +1,22 @@
 IF NOT EXISTS (select * from dbo.sysobjects where id = object_id(N'[House]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
 BEGIN
-	CREATE TABLE [House]
-	(	[ID] [int] IDENTITY(1,1) NOT NULL
-	,	[HouseNr] [varchar](10) NOT NULL
-	,	[KadastrNr] [varchar](10)
-	,	[BuildNr] [varchar](10) NOT NULL
-	,	[AddressID] [int] NOT NULL
-	,	[PhoneNr] [varchar](12) NULL
-	,	[PhoneCode] [varchar](5) NULL
-	,	[FaxNr] [varchar](12) NULL
-	,	[Year] [int] NULL
-	,	[Code] [varchar](50) NULL
-	,	[LastUpdDT] [smalldatetime] NOT NULL
-	,	[LastUpdUS] [varchar](50) NOT NULL	
-	, CONSTRAINT [SYS_House_PKY] PRIMARY KEY CLUSTERED 
-		  (
-			  [ID] ASC
-		  ) WITH  FILLFACTOR = 95  ON [PRIMARY]
-	)   ON [PRIMARY]
+  CREATE TABLE [House]
+  ( [ID] [int] IDENTITY(1,1) NOT NULL
+  , [HouseNr] [varchar](10) NOT NULL
+  , [KadastrNr] [varchar](10)
+  , [BuildNr] [varchar](10) NOT NULL
+  , [AddressID] [int] NOT NULL
+  , [PhoneNr] [varchar](12) NULL
+  , [PhoneCode] [varchar](5) NULL
+  , [FaxNr] [varchar](12) NULL
+  , [Year] [int] NULL
+  , [Code] [varchar](50) NULL
+  , [LastUpdDT] [smalldatetime] NOT NULL
+  , [LastUpdUS] [varchar](50) NOT NULL	
+  , CONSTRAINT [SYS_House_PKY] PRIMARY KEY CLUSTERED 
+    (
+      [ID] ASC
+    ) WITH  FILLFACTOR = 95  ON [PRIMARY]
+  )   ON [PRIMARY]
 END
 GO
