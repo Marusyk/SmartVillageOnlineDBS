fbda4e734cd5ef9baefe45cdd3ad49468aca12cd
Roma <romamarusyk@gmail.com>
2015-08-13 01:13:40 +0300

    Added variable runsqlcmd for more simple using sqlcmd

--- MigrationDB.bat
+++ MigrationDB.bat
@@ -5,6 +5,7 @@ SET server=MARUSYK\SQLEXPRESS
 SET database=SVData 
 SET username=sa
 SET password=sa#001
+SET runsqlcmd=sqlcmd -S %server% -U %username% -P %password% -d %database% -i
 
 rem FOR DEPLOY ENV.
 rem SET server=b32c2b3e-50c3-4d45-a886-a4ba00dfb377.sqlserver.sequelizer.com
@@ -21,45 +22,45 @@ if /I "%PARAM_VALUE%"=="n" goto CleanUpData
 goto scripts
 
 :CleanUpObjects
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "31.07.2015 - RM - CleanUpObjects.sql"
+%runsqlcmd% "31.07.2015 - RM - CleanUpObjects.sql"
 goto scripts
 :CleanUpData
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "30.07.2015 - RM - CleanUpData.sql"
+%runsqlcmd% "30.07.2015 - RM - CleanUpData.sql"
 goto scripts
 
 :scripts
 cd ../15.Tables
-for %%G in (*.sql) do sqlcmd -S %server% -U %username% -P %password% -d %database% -i "%%G"
+for %%G in (*.sql) do %runsqlcmd% "%%G"
 
 cd ../20.Indexes
-for %%G in (*.sql) do sqlcmd -S %server% -U %username% -P %password% -d %database% -i "%%G"
+for %%G in (*.sql) do %runsqlcmd% "%%G"
 
 cd ../35.ForeignConstraints
-for %%G in (*.sql) do sqlcmd -S %server% -U %username% -P %password% -d %database% -i "%%G"
+for %%G in (*.sql) do %runsqlcmd% "%%G"
 
 cd ../45.Views
-for %%G in (*.sql) do sqlcmd -S %server% -U %username% -P %password% -d %database% -i "%%G"
+for %%G in (*.sql) do %runsqlcmd% "%%G"
 
 cd ../40.Data
 rem In order dependencies
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "SYS_Dictionary.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Animals.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Catalog.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "DocumentType.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "FamilyRelations.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "FamilyStatus.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Materials.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Nationality.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Country.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Region.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "District.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "CityType.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "StreetType.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "City.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Street.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Address.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "House.sql"
-sqlcmd -S %server% -U %username% -P %password% -d %database% -i "Person.sql"
+%runsqlcmd% "SYS_Dictionary.sql"
+%runsqlcmd% "Animals.sql"
+%runsqlcmd% "Catalog.sql"
+%runsqlcmd% "DocumentType.sql"
+%runsqlcmd% "FamilyRelations.sql"
+%runsqlcmd% "FamilyStatus.sql"
+%runsqlcmd% "Materials.sql"
+%runsqlcmd% "Nationality.sql"
+%runsqlcmd% "Country.sql"
+%runsqlcmd% "Region.sql"
+%runsqlcmd% "District.sql"
+%runsqlcmd% "CityType.sql"
+%runsqlcmd% "StreetType.sql"
+%runsqlcmd% "City.sql"
+%runsqlcmd% "Street.sql"
+%runsqlcmd% "Address.sql"
+%runsqlcmd% "House.sql"
+%runsqlcmd% "Person.sql"
 
 cd..
 pause
