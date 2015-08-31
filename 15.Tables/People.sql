c4479fc36390c9d77152bf0a33a4d1d48a8f6ae9
Roma <romamarusyk@gmail.com>
2015-08-31 22:19:42 +0300

    Created table People. Task Marusyk/SmartVillageOnline#16

---
+++ 15.Tables/People.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS (select * from dbo.sysobjects where id = object_id(N'[People]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+  CREATE TABLE [People]
+  ( [ID] [int] IDENTITY(1,1) NOT NULL
+  , [PersonId] [int] NOT NULL
+  , [HouseId] [int] NOT NULL
+  , [PeopleNumber] [int] NOT NULL
+  , [FamilyRelationId] [int] NULL
+  , [DateRegistration] [date] NULL
+  , [IsMain] [bit] NOT NULL DEFAULT (0)
+  , [LastUpdDT] [smalldatetime] NOT NULL
+  , [LastUpdUS] [nvarchar](50) NOT NULL	
+  , CONSTRAINT [SYS_People_PKY] PRIMARY KEY CLUSTERED 
+    (
+      [ID] ASC
+    ) WITH  FILLFACTOR = 95  ON [PRIMARY]
+  )   ON [PRIMARY]
+END
+GO
