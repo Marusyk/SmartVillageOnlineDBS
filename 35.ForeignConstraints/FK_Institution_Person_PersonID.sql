f1f83089bf2f32e86f743f1ef0694f6a119efb2d
Roma <romamarusyk@gmail.com>
2015-09-25 01:52:36 +0300

    Added FK to PersonDocuments. Task #6

---
+++ 35.ForeignConstraints/FK_Institution_Person_PersonID.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS ( SELECT * FROM sysObjects WHERE xtype LIKE 'F' AND name LIKE 'FK_Institution_Person_PersonID')
+BEGIN
+	ALTER TABLE [dbo].[PersonDocuments] ADD CONSTRAINT [FK_Institution_Person_PersonID] FOREIGN KEY 
+		(
+			[PersonID]
+		) REFERENCES [Person] (
+			[ID]
+		)
+END
+GO
---
+++ 35.ForeignConstraints/FK_PersonDocuments_Document_DocumentID.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS ( SELECT * FROM sysObjects WHERE xtype LIKE 'F' AND name LIKE 'FK_PersonDocuments_Document_DocumentID')
+BEGIN
+	ALTER TABLE [dbo].[PersonDocuments] ADD CONSTRAINT [FK_PersonDocuments_Document_DocumentID] FOREIGN KEY 
+		(
+			[DocumentID]
+		) REFERENCES [Document] (
+			[ID]
+		)
+END
+GO
