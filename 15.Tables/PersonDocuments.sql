8b69c0c45293f30c242f03151d5128f9533234eb
Roma <romamarusyk@gmail.com>
2015-09-25 01:52:16 +0300

    Added PersonDocuments. Task #6

---
+++ 15.Tables/PersonDocuments.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (select * from dbo.sysobjects where id = object_id(N'[PersonDocuments]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+  CREATE TABLE [PersonDocuments]
+  ( [ID] [int] IDENTITY(1,1) NOT NULL
+  , [PersonID] [int] NULL
+  , [DocumentID] [int] NULL
+  , [LastUpdDT] [smalldatetime] NOT NULL
+  , [LastUpdUS] [nvarchar](50) NOT NULL
+  , CONSTRAINT [SYS_PersonDocuments_PKY] PRIMARY KEY CLUSTERED 
+    (
+      [ID] ASC
+    ) WITH  FILLFACTOR = 95  ON [PRIMARY]
+  )   ON [PRIMARY]
+END
+GO
