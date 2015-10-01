edcf9dc81a36aed44f94f433a20ea8f6ea2be5cd
Roma <romamarusyk@gmail.com>
2015-10-02 02:12:42 +0300

    Fixed bug with name of table.

---
+++ 35.ForeignConstraints/FK_Employment_Companies_CompanyID.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS ( SELECT * FROM sysObjects WHERE xtype LIKE 'F' AND name LIKE 'FK_Employment_Companies_CompanyID')
+BEGIN
+	ALTER TABLE [dbo].[Employment] ADD CONSTRAINT [FK_Employment_Companies_CompanyID] FOREIGN KEY 
+		(
+			[CompanyID]
+		) REFERENCES [Companies] (
+			[ID]
+		)
+END
+GO
